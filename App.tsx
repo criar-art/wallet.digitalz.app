1fb085263ee1a0b4d818b3d3bab80a8e7c39cc6d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-18 08:22:00 -0300

    config tailwind native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View testID="app-container" style={styles.container}>
-      <Text>wallet.digitalz.app</Text>
+    <View
+      testID="app-container"
+      className="flex-1 items-center justify-center bg-red-600"
+    >
+      <Text className="text-white">wallet.digitalz.app</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
