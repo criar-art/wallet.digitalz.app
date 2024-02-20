72502f49d8f76dea0631f9b5d22fb5a24ac8de49
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-20 14:34:05 -0300

    adjustment props of components

--- src/components/ListRegisters/types.ts
+++ src/components/ListRegisters/types.ts
@@ -1,11 +1,4 @@
-import { GestureResponderEvent } from "react-native";
-
 export type Props = {
   testID?: string;
-  backgroundColor?: string;
-  textColor?: string;
   className?: string;
-  text?: string;
-  icon?: React.JSX.Element;
-  onPress?: ((event: GestureResponderEvent) => void);
 }
