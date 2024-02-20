97ed53297c7efdba75de68c1a7b0f9aca8823397
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-19 21:34:32 -0300

    adjustment modal with KeyboardAvoidingView

--- src/views/HomeScreen.tsx
+++ src/views/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import {
   Alert,
+  KeyboardAvoidingView,
   Modal,
   Pressable,
   Text,
@@ -171,116 +172,123 @@ export default function Home() {
           setModalVisible(!modalVisible);
         }}
       >
-        <View className="bg-white p-4 rounded-lg border-2 border-slate-400 m-10">
-          <TouchableOpacity
-            className="scale-75 z-20 absolute top-0 right-0 m-2 flex justify-center items-center w-10 bg-red-600 rounded-full p-2 text-center"
-            onPress={() => setModalVisible(!modalVisible)}
-          >
-            <MaterialIcons name="close" size={22} color="white" />
-          </TouchableOpacity>
-          <Text className="text-black text-center mb-5 border-b-2 pb-4 border-slate-300">
-            Criar Novo Registro
-          </Text>
-          <Text className="text-black mb-2">Tipo</Text>
-          <Dropdown
-            style={{
-              marginBottom: 20,
-              paddingHorizontal: 10,
-              borderRadius: 8,
-              borderColor: "#94a3b8",
-              borderWidth: 2,
-            }}
-            containerStyle={{
-              paddingVertical: 2.5,
-              borderRadius: 8,
-              borderColor: "#94a3b8",
-              borderWidth: 2,
-              backgroundColor: "#fff",
-            }}
-            itemContainerStyle={{
-              borderRadius: 6,
-              marginHorizontal: 5,
-              marginVertical: 2.5,
-              backgroundColor: "#eee",
-              padding: 0,
-              height: 45,
-            }}
-            itemTextStyle={{
-              height: 20,
-              margin: 0,
-              padding: 0,
-              position: "relative",
-              top: -5,
-            }}
-            activeColor="#dcfce7"
-            data={dataType}
-            maxHeight={300}
-            labelField="label"
-            valueField="value"
-            placeholder="Selecione o tipo"
-            value={formModal.type}
-            onChange={({ value }) => handleChange(value, "type")}
-          />
-          {showDate && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={date}
-              mode="date"
-              is24Hour={true}
-              onChange={onChange}
-            />
-          )}
-          <Text className="text-black mb-2">Data</Text>
-          <Pressable
-            onPress={() => setShowDate(true)}
-            className="flex flex-row mb-4 p-1 px-2 bg-white rounded-lg border-2 border-slate-400"
-          >
-            <MaterialIcons name="calendar-month" size={22} color="black" />
-            <TextInput
-              className="ml-2 text-black"
-              placeholder="Data do registro"
-              onChangeText={(value: string) => handleChange(value, "date")}
-              value={formModal.date}
-              editable={false}
-            />
-          </Pressable>
-          <Text className="text-black mb-2">Nome</Text>
-          <TextInput
-            className="mb-4 p-1 px-2 bg-white rounded-lg border-2 border-slate-400"
-            placeholder="Nome do registro"
-            onChangeText={(value: string) => handleChange(value, "name")}
-            value={formModal.name}
-          />
-          <Text className="text-black mb-2">Valor</Text>
-          <TextInput
-            className="mb-4 p-1 px-2 bg-white rounded-lg border-2 border-slate-400"
-            placeholder="Valor do registro"
-            onChangeText={(value: string) => handleChange(value, "value")}
-            value={formModal.value}
-          />
-          <View className="flex flex-row">
+        <KeyboardAvoidingView behavior="padding" className="flex justify-center translate-y-[-40px]">
+          <View className="bg-white p-4 rounded-lg border-2 border-slate-400 m-10">
             <TouchableOpacity
-              className="flex flex-1 justify-center items-center flex-row bg-gray-600 rounded-lg p-2 text-center mr-1"
-              onPress={() => {
-                setModalVisible(false);
+              className="scale-75 z-20 absolute top-0 right-0 m-2 flex justify-center items-center w-10 bg-red-600 rounded-full p-2 text-center"
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <MaterialIcons name="close" size={22} color="white" />
+            </TouchableOpacity>
+            <Text className="text-black text-center mb-5 border-b-2 pb-4 border-slate-300">
+              Criar Novo Registro
+            </Text>
+            <Text className="text-black mb-2">Tipo</Text>
+            <Dropdown
+              style={{
+                marginBottom: 20,
+                paddingHorizontal: 10,
+                borderRadius: 8,
+                borderColor: "#94a3b8",
+                borderWidth: 2,
               }}
+              containerStyle={{
+                paddingVertical: 2.5,
+                borderRadius: 8,
+                borderColor: "#94a3b8",
+                borderWidth: 2,
+                backgroundColor: "#fff",
+              }}
+              itemContainerStyle={{
+                borderRadius: 6,
+                marginHorizontal: 5,
+                marginVertical: 2.5,
+                backgroundColor: "#eee",
+                padding: 0,
+                height: 45,
+              }}
+              itemTextStyle={{
+                height: 20,
+                margin: 0,
+                padding: 0,
+                position: "relative",
+                top: -5,
+              }}
+              activeColor="#dcfce7"
+              data={dataType}
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder="Selecione o tipo"
+              value={formModal.type}
+              onChange={({ value }) => handleChange(value, "type")}
+            />
+            {showDate && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode="date"
+                is24Hour={true}
+                onChange={onChange}
+              />
+            )}
+            <Text className="text-black mb-2">Data</Text>
+            <Pressable
+              onPress={() => setShowDate(true)}
+              className="flex flex-row mb-4 p-1 px-2 bg-white rounded-lg border-2 border-slate-400"
             >
-              <>
-                <MaterialCommunityIcons name="cancel" size={22} color="white" />
-                <Text className="ml-2 text-center text-white">Cancelar</Text>
-              </>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className="flex flex-1 justify-center items-center flex-row bg-green-600 rounded-lg p-2 text-center ml-1"
-              onPress={() => saveStore()}
-            >
-              <>
-                <MaterialIcons name="save" size={22} color="white" />
-                <Text className="ml-2 text-center text-white">Salvar</Text>
-              </>
-            </TouchableOpacity>
+              <MaterialIcons name="calendar-month" size={22} color="black" />
+              <TextInput
+                className="ml-2 text-black"
+                placeholder="Data do registro"
+                onChangeText={(value: string) => handleChange(value, "date")}
+                value={formModal.date}
+                editable={false}
+              />
+            </Pressable>
+            <Text className="text-black mb-2">Nome</Text>
+            <TextInput
+              className="mb-4 p-1 px-2 bg-white rounded-lg border-2 border-slate-400"
+              placeholder="Nome do registro"
+              onChangeText={(value: string) => handleChange(value, "name")}
+              value={formModal.name}
+            />
+            <Text className="text-black mb-2">Valor</Text>
+            <TextInput
+              className="mb-4 p-1 px-2 bg-white rounded-lg border-2 border-slate-400"
+              placeholder="Valor do registro"
+              onChangeText={(value: string) => handleChange(value.replace(/[^0-9]/g, '') , "value")}
+              value={formModal.value}
+              keyboardType='numeric'
+            />
+            <View className="flex flex-row">
+              <TouchableOpacity
+                className="flex flex-1 justify-center items-center flex-row bg-gray-600 rounded-lg p-2 text-center mr-1"
+                onPress={() => {
+                  setModalVisible(false);
+                }}
+              >
+                <>
+                  <MaterialCommunityIcons
+                    name="cancel"
+                    size={22}
+                    color="white"
+                  />
+                  <Text className="ml-2 text-center text-white">Cancelar</Text>
+                </>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="flex flex-1 justify-center items-center flex-row bg-green-600 rounded-lg p-2 text-center ml-1"
+                onPress={() => saveStore()}
+              >
+                <>
+                  <MaterialIcons name="save" size={22} color="white" />
+                  <Text className="ml-2 text-center text-white">Salvar</Text>
+                </>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
+        </KeyboardAvoidingView>
       </Modal>
     </View>
   );
