e12a55e71bed346921182102a4207bc977e8a7d6
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-20 14:32:52 -0300

    create utils

---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+export const renderBorderType = (type: string) => {
+  switch (type) {
+    case "investiment":
+      return "border-sky-400";
+    case "entry":
+      return "border-green-400";
+    case "expense":
+      return "border-red-400";
+    case "vehicle":
+      return "border-gray-400";
+  }
+};
