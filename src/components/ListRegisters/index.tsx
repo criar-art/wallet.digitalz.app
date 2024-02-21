06e987a06470d1a5ecea88848bdf34004013c0b3
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-21 06:20:40 -0300

    clean useState

--- src/components/ListRegisters/index.tsx
+++ src/components/ListRegisters/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Text, View } from "react-native";
 import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { renderBorderType } from "../../utils";
