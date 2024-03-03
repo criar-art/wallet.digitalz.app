57b8d1a92009126f32aa44250cba629d6065d292
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-03-02 21:09:01 -0300

    test(unit): improvement spec screens About and Contact

--- src/views/ContactScreen.test.spec.tsx
+++ src/views/ContactScreen.test.spec.tsx
@@ -2,7 +2,18 @@ import { render } from '@testing-library/react-native';
 import ContactScreen from './ContactScreen';
 
 test('should render ContactScreen', () => {
-  const { getByTestId } = render(<ContactScreen />);
+  const { getByTestId, getByText } = render(<ContactScreen />);
 
   expect(getByTestId('contact-screen')).toBeTruthy();
+
+  // Check if the expected text content is present
+  const text1 = getByText('Se você tem algum feedback ou precisa de ajuda.');
+  const text2 = getByText('Entre em contato conosco.');
+  const emailText = getByText('contato@criar.art');
+  const linkedinText = getByText('linkedin.com/in/lucasferreiralimax');
+
+  expect(text1).toBeTruthy();
+  expect(text2).toBeTruthy();
+  expect(emailText).toBeTruthy();
+  expect(linkedinText).toBeTruthy();
 });
