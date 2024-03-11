723c6cfd61e3a6c6ad03557d92d54260a873f79f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-03-11 17:28:03 -0300

    show or not modalRegister in Routes

--- App.tsx
+++ App.tsx
@@ -1,26 +1,47 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import store from "./src/store";
-
 import Routes from "./src/routes/views";
 import ModalRegister from "./src/components/ModalRegister";
 
 export default function App() {
   const persistor = persistStore(store);
+  const navigationRef = useNavigationContainerRef();
+  const [modalRegister, setModalRegister] = useState(false);
+
+  function checkRoute() {
+    if (navigationRef.isReady()) {
+      const currentRoute = navigationRef.getCurrentRoute();
+      setModalRegister(
+        currentRoute?.name == "About" || currentRoute?.name == "Contact"
+      );
+    }
+  }
+
+  useEffect(() => {
+    checkRoute();
+  }, []);
 
   return (
     <SafeAreaProvider testID="app-container">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <NavigationContainer>
+          <NavigationContainer
+            ref={navigationRef}
+            onStateChange={() => checkRoute()}
+          >
             <StatusBar style="auto" />
             <Routes />
-            <ModalRegister />
+            {!modalRegister && <ModalRegister />}
           </NavigationContainer>
         </PersistGate>
       </Provider>
