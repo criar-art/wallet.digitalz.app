1d39dbfb4faf15086aa4d7513e0b61fa812082bb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-11 15:51:43 -0300

    test(unit): adjustment views with navigation

--- src/views/AboutScreen.test.spec.tsx
+++ src/views/AboutScreen.test.spec.tsx
@@ -1,15 +1,26 @@
-import { render } from '@testing-library/react-native';
-import AboutScreen from './AboutScreen';
+import { render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import AboutScreen from "./AboutScreen";
 
-test('should render AboutScreen', () => {
-  const { getByTestId, getByText } = render(<AboutScreen />);
+test("should render AboutScreen", () => {
+  const { getByTestId, getByText } = render(
+    <NavigationContainer>
+      <AboutScreen />
+    </NavigationContainer>
+  );
 
-  expect(getByTestId('about-screen')).toBeTruthy();
+  expect(getByTestId("about-screen")).toBeTruthy();
 
   // Check if the expected text content is present
-  const text1 = getByText('Wallet Digitalz foi desenvolvido com o objetivo de auxiliá-lo na organização das suas finanças.');
-  const text2 = getByText('Proporcionamos uma maneira simplificada para que você registre suas despesas e valores.');
-  const text3 = getByText('Tenha a liberdade de criar seus registros de forma intuitiva, com a flexibilidade de deletá-los e atualizá-los conforme necessário.');
+  const text1 = getByText(
+    "Wallet Digitalz foi desenvolvido com o objetivo de auxiliá-lo na organização das suas finanças."
+  );
+  const text2 = getByText(
+    "Proporcionamos uma maneira simplificada para que você registre suas despesas e valores."
+  );
+  const text3 = getByText(
+    "Tenha a liberdade de criar seus registros de forma intuitiva, com a flexibilidade de deletá-los e atualizá-los conforme necessário."
+  );
 
   expect(text1).toBeTruthy();
   expect(text2).toBeTruthy();
--- src/views/ContactScreen.test.spec.tsx
+++ src/views/ContactScreen.test.spec.tsx
@@ -1,16 +1,21 @@
-import { render } from '@testing-library/react-native';
-import ContactScreen from './ContactScreen';
+import { render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import ContactScreen from "./ContactScreen";
 
-test('should render ContactScreen', () => {
-  const { getByTestId, getByText } = render(<ContactScreen />);
+test("should render ContactScreen", () => {
+  const { getByTestId, getByText } = render(
+    <NavigationContainer>
+      <ContactScreen />
+    </NavigationContainer>
+  );
 
-  expect(getByTestId('contact-screen')).toBeTruthy();
+  expect(getByTestId("contact-screen")).toBeTruthy();
 
   // Check if the expected text content is present
-  const text1 = getByText('Se você tem algum feedback ou precisa de ajuda.');
-  const text2 = getByText('Entre em contato conosco.');
-  const emailText = getByText('contato@criar.art');
-  const linkedinText = getByText('linkedin.com/in/lucasferreiralimax');
+  const text1 = getByText("Se você tem algum feedback ou precisa de ajuda.");
+  const text2 = getByText("Entre em contato conosco.");
+  const emailText = getByText("contato@criar.art");
+  const linkedinText = getByText("linkedin.com/in/lucasferreiralimax");
 
   expect(text1).toBeTruthy();
   expect(text2).toBeTruthy();
