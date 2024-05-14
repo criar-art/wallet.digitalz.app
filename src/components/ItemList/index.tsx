cf1741e871d7b415ecd4944ed7c9b854875b0825
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-14 05:43:28 -0300

    improvement size itemList

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Text, View, Animated, TouchableOpacity } from "react-native";
-import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons, MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
 import { NumericFormat } from "react-number-format";
 import { renderBorderType } from "../../utils";
 import { Props } from "./types";
@@ -34,18 +34,17 @@ export default function ItemList(props: Props) {
 
   return (
     <TouchableOpacity
+      key={props.item.id}
       testID={props.testID}
       onPress={() => setOptionsShow(!optionsShow)}
+      className={`border-l-4 text-black mt-5 mx-5 bg-white p-6 pt-4 rounded-lg shadow-lg ${renderBorderType(
+        props.item.type
+      )}`}
     >
-      <View
-        key={props.item.id}
-        className={`border-l-4 text-black mt-5 mx-5 bg-white p-4 rounded-lg shadow-lg ${renderBorderType(
-          props.item.type
-        )}`}
-      >
-        <Text className="mb-2 text-black">{props.item.name}</Text>
-        <Text className="mb-2 text-black">
-          Valor:{" "}
+      <Text className="mb-2 text-black text-xl">{props.item.name}</Text>
+      <View className="flex flex-row items-center mb-2">
+        <FontAwesome5 name="money-bill-wave" size={16} color="black" />
+        <Text className="ml-2 text-black">
           <NumericFormat
             value={props.item.value}
             displayType={"text"}
@@ -59,37 +58,37 @@ export default function ItemList(props: Props) {
             )}
           />
         </Text>
-        <View className="flex flex-row items-center">
-          <MaterialIcons name="calendar-month" size={22} color="black" />
-          <Text className="ml-2 text-black">Data: {props.item.date}</Text>
-        </View>
-        <Animated.View
-          className="flex flex-row items-center z-20 absolute top-0 right-0 bottom-0"
-          style={{ opacity: fadeAnim }}
-          pointerEvents={!optionsShow ? "none" : "auto"}
-        >
-          <Button
-            backgroundColor="bg-white"
-            className="z-20 w-14 h-14 my-5 rounded-full border-2 border-gray-200"
-            onPress={props.edit}
-            label={`Editar registro ${props.item.name}`}
-            icon={<MaterialIcons name="edit" size={22} color="black" />}
-          />
-          <Button
-            backgroundColor="bg-white"
-            className="z-20 w-14 h-14 m-5 rounded-full border-2 border-red-200"
-            onPress={props.remove}
-            label={`Excluir registro ${props.item.name}`}
-            icon={
-              <MaterialCommunityIcons
-                name="trash-can-outline"
-                size={22}
-                color="red"
-              />
-            }
-          />
-        </Animated.View>
       </View>
+      <View className="flex flex-row items-center">
+        <MaterialIcons name="calendar-month" size={22} color="black" />
+        <Text className="ml-2 text-black">{props.item.date}</Text>
+      </View>
+      <Animated.View
+        className="flex flex-row items-center z-20 absolute top-0 right-0 bottom-0"
+        style={{ opacity: fadeAnim }}
+        pointerEvents={!optionsShow ? "none" : "auto"}
+      >
+        <Button
+          backgroundColor="bg-white"
+          className="z-20 w-14 h-14 my-5 rounded-full border-2 border-gray-200"
+          onPress={props.edit}
+          label={`Editar registro ${props.item.name}`}
+          icon={<MaterialIcons name="edit" size={22} color="black" />}
+        />
+        <Button
+          backgroundColor="bg-white"
+          className="z-20 w-14 h-14 m-5 rounded-full border-2 border-red-200"
+          onPress={props.remove}
+          label={`Excluir registro ${props.item.name}`}
+          icon={
+            <MaterialCommunityIcons
+              name="trash-can-outline"
+              size={22}
+              color="red"
+            />
+          }
+        />
+      </Animated.View>
     </TouchableOpacity>
   );
 }
