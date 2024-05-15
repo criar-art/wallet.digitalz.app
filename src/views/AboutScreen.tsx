31f6aecbd7a24e90446b4b1d7a52a8b9458a8116
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-15 02:37:32 -0300

    improvement views About and Contact

--- src/views/AboutScreen.tsx
+++ src/views/AboutScreen.tsx
@@ -1,28 +1,52 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, ScrollView, Linking } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
 import FadeView from "../components/FadeView";
+import Button from "../components/Button";
 
 export default function AboutScreen() {
   return (
     <FadeView>
-      <View testID="about-screen" className="p-5">
-        <Text className="my-2 text-black text-base">
-          Wallet Digitalz foi desenvolvido com o objetivo de auxiliá-lo na
-          organização das suas finanças.
-        </Text>
-        <Text className="my-2 text-black text-base">
-          Proporcionamos uma maneira simplificada para que você registre suas
-          despesas e valores.
-        </Text>
-        <Text className="my-2 text-black text-base">
-          Tenha a liberdade de criar seus registros de forma intuitiva, com a
-          flexibilidade de deletá-los e atualizá-los conforme necessário.
-        </Text>
+      <View testID="about-screen" className="flex flex-col h-full">
         <Image
-          className="mt-5 rounded-lg"
           style={{ resizeMode: "stretch", height: 250, width: "100%" }}
           source={require("../images/finance.jpg")}
         />
+        <ScrollView
+          className="p-5"
+          contentContainerStyle={{
+            flexGrow: 1,
+          }}
+        >
+          <Text className="my-2 text-black text-base">
+            Wallet Digitalz foi desenvolvido com o objetivo de auxiliá-lo na
+            organização das suas finanças.
+          </Text>
+          <Text className="my-2 text-black text-base">
+            Proporcionamos uma maneira simplificada para que você registre suas
+            despesas e valores.
+          </Text>
+          <Text className="my-2 text-black text-base">
+            Tenha a liberdade de criar seus registros de forma intuitiva, com a
+            flexibilidade de deletá-los e atualizá-los conforme necessário.
+          </Text>
+          <Button
+            text="walletdigitalz.web.app"
+            label="Acessar site walletdigitalz.web.app"
+            textColor="text-black"
+            onPress={() => Linking.openURL("https://walletdigitalz.web.app")}
+            icon={<Fontisto name="world" size={28} color="black" />}
+            className="justify-start mt-4 p-6 shadow-lg bg-white"
+          />
+          <Button
+            text="criar.art"
+            label="Acessar site criar.art"
+            textColor="text-black"
+            onPress={() => Linking.openURL("https://criar.art/contact")}
+            icon={<Fontisto name="world" size={28} color="black" />}
+            className="justify-start mt-4 mb-4 p-6 shadow-lg bg-white"
+          />
+        </ScrollView>
       </View>
     </FadeView>
   );
--- src/views/ContactScreen.tsx
+++ src/views/ContactScreen.tsx
@@ -1,4 +1,4 @@
-import { Image, Linking, Text, TouchableOpacity, View } from "react-native";
+import { Image, Linking, View, ScrollView } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Zocial } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
@@ -9,46 +9,58 @@ import Button from "../components/Button";
 export default function ContactScreen() {
   return (
     <FadeView>
-      <View testID="contact-screen" className="p-5">
-        <Text className="my-2 text-black text-base">
-          Se precisa de ajuda ou feedback.
-        </Text>
-        <Text className="my-2 text-black text-base">Entre em contato.</Text>
-        <Button
-          text="criar.art"
-          label="Acessar site criar.art"
-          textColor="text-black"
-          onPress={() => Linking.openURL("https://criar.art/contact")}
-          icon={<Fontisto name="world" size={28} color="black" />}
-          className="justify-start mt-4 p-6 shadow-lg bg-white"
-        />
-        <Button
-          text="contato@criar.art"
-          label="Enviar email para contato@criar.art"
-          textColor="text-black"
-          onPress={() =>
-            Linking.openURL(
-              "mailto:contato@criar.art?subject=Wallet Digitalz APP&body=Olá tudo bem?\nPreciso de ajuda com"
-            )
-          }
-          icon={<MaterialIcons name="email" size={28} color="black" />}
-          className="justify-start mt-4 p-6 shadow-lg bg-white"
-        />
-        <Button
-          text="@lucasferreiralimax"
-          label="Acessar linkedin do Lucas Ferreira"
-          textColor="text-black"
-          onPress={() =>
-            Linking.openURL("https://linkedin.com/in/lucasferreiralimax")
-          }
-          icon={<Zocial name="linkedin" size={28} color="rgb(10, 102, 194)" />}
-          className="justify-start mt-4 p-6 shadow-lg bg-white"
-        />
+      <View testID="contact-screen" className="flex flex-col h-full">
         <Image
-          className="mt-5 rounded-lg"
           style={{ resizeMode: "stretch", height: 250, width: "100%" }}
           source={require("../images/world.png")}
         />
+        <ScrollView
+          className="px-5"
+          contentContainerStyle={{
+            flexGrow: 1,
+          }}
+        >
+          <Button
+            text="criar.art"
+            label="Acessar site criar.art"
+            textColor="text-black"
+            onPress={() => Linking.openURL("https://criar.art/contact")}
+            icon={<Fontisto name="world" size={28} color="black" />}
+            className="justify-start mt-4 p-6 shadow-lg bg-white"
+          />
+          <Button
+            text="criar.art.tecnologia"
+            label="Acessar Instagram criar.art.tecnologia"
+            textColor="text-black"
+            onPress={() =>
+              Linking.openURL("http://instagram.com/criar.art.tecnologia")
+            }
+            icon={<Zocial name="instagram" size={28} color="black" />}
+            className="justify-start mt-4 p-6 shadow-lg bg-white"
+          />
+          <Button
+            text="contato@criar.art"
+            label="Enviar email para contato@criar.art"
+            textColor="text-black"
+            onPress={() =>
+              Linking.openURL(
+                "mailto:contato@criar.art?subject=Wallet Digitalz APP&body=Olá tudo bem?\nPreciso de ajuda com"
+              )
+            }
+            icon={<MaterialIcons name="email" size={28} color="black" />}
+            className="justify-start mt-4 p-6 shadow-lg bg-white"
+          />
+          <Button
+            text="@lucasferreiralimax"
+            label="Acessar linkedin do Lucas Ferreira"
+            textColor="text-black"
+            onPress={() =>
+              Linking.openURL("https://linkedin.com/in/lucasferreiralimax")
+            }
+            icon={<Zocial name="linkedin" size={28} color="black" />}
+            className="justify-start mt-4 p-6 shadow-lg bg-white"
+          />
+        </ScrollView>
       </View>
     </FadeView>
   );
