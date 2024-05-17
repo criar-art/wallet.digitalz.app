e414fadf7c66eb4e210146706ae1ed5caba2f0f4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-17 00:05:11 -0300

    refactory AppDrawerContent

--- src/components/AppDrawerContent/index.tsx
+++ src/components/AppDrawerContent/index.tsx
@@ -2,7 +2,7 @@ import { Text, View, Platform } from "react-native";
 import pkg from "../../../package.json";
 import app from "../../../app.json";
 import Button from "../Button";
-import { Props } from "./types";
+import { Props, RenderButtonParams } from "./types";
 
 export default function AppDrawerContent({
   state,
@@ -10,58 +10,74 @@ export default function AppDrawerContent({
   navigation,
 }: Props) {
   const appVersion = pkg.version;
-  const versionCode = app.expo.android.versionCode;
+  const versionCode =
+    Platform.OS === "android" ? app.expo.android.versionCode : "";
+
+  const getLabel = (options: any, route: any): string =>
+    options.tabBarLabel !== undefined
+      ? options.tabBarLabel
+      : options.title !== undefined
+      ? options.title
+      : route.name;
+
+  const onPress = (route: any, isFocused: boolean): void => {
+    if (!isFocused) {
+      navigation.navigate(route.name, route.params);
+    } else {
+      navigation.closeDrawer();
+    }
+  };
+
+  const renderButton = ({
+    route,
+    index,
+    isFocused,
+    labelButton,
+    options,
+  }: RenderButtonParams): JSX.Element => (
+    <Button
+      key={index}
+      accessibilityState={isFocused ? { selected: true } : {}}
+      text={labelButton}
+      label={`Navegar para ${labelButton}`}
+      onPress={() => onPress(route, isFocused)}
+      className={`justify-start py-7 ml-2 pl-6 bg-transparent rounded-none ${
+        isFocused ? "bg-gray-200 rounded-tl-full rounded-bl-full" : ""
+      }`}
+      textColor="black"
+      icon={
+        <View
+          className={`flex items-center mr-4 ${
+            !isFocused ? "scale-100" : "scale-125"
+          }`}
+        >
+          {<options.drawerIcon size={28} color="black" />}
+        </View>
+      }
+    />
+  );
 
   return (
     <View testID="app-drawer-content" className="flex flex-row w-full h-full">
       <View className="pt-12 w-full">
         {state?.routes.map((route: any, index: number) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
+          const { options }: { options: any } = descriptors[route.key];
+          const labelButton: string = getLabel(options, route);
+          const isFocused: boolean = state.index === index;
 
-          const labelButton = label == "Wallet Digitalz" ? "Ínicio" : label;
-          const isFocused = state.index === index;
-
-          const onPress = () => {
-            if (!isFocused) {
-              navigation.navigate(route.name, route.params);
-            } else {
-              navigation.closeDrawer();
-            }
-          };
-
-          return (
-            <Button
-              key={index}
-              accessibilityState={isFocused ? { selected: true } : {}}
-              text={labelButton}
-              label={`Navegar para ${labelButton}`}
-              onPress={onPress}
-              className={`justify-start py-7 ml-2 pl-6 bg-transparent rounded-none ${
-                !!isFocused && "bg-gray-200 rounded-tl-full rounded-bl-full"
-              }`}
-              textColor="black"
-              icon={
-                <View
-                  className={`flex items-center mr-4 ${
-                    !isFocused ? "scale-100" : "scale-125"
-                  }`}
-                >
-                  {<options.drawerIcon size={28} color="black" />}
-                </View>
-              }
-            ></Button>
-          );
+          return renderButton({
+            route,
+            index,
+            isFocused,
+            labelButton,
+            options,
+          });
         })}
+
         <View className="flex p-10 mt-auto">
           <Text className="text-center">
             Versão {appVersion}
-            {Platform.OS === "android" && ` - Build ${versionCode}`}
+            {versionCode && ` - Build ${versionCode}`}
           </Text>
         </View>
       </View>
--- src/components/AppDrawerContent/types.ts
+++ src/components/AppDrawerContent/types.ts
@@ -2,4 +2,12 @@ export type Props = {
   state?: any;
   navigation?: any;
   descriptors?: any;
-}
+};
+
+export type RenderButtonParams = {
+  route: any;
+  index: number;
+  isFocused: boolean;
+  labelButton: string;
+  options: any;
+};
