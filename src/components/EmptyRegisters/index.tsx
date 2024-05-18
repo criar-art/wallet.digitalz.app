0ac80bbba8020a8ff1f4bfa012e46882cc1706a0
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-18 11:48:18 -0300

    adjustment darkmode in emptyRegisters

--- src/components/EmptyRegisters/index.tsx
+++ src/components/EmptyRegisters/index.tsx
@@ -1,8 +1,11 @@
 import { Text, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useColorScheme } from "nativewind";
 import { Props } from "./types";
 
 export default function EmptyRegisters(props: Props) {
+  const { colorScheme } = useColorScheme();
+
   return (
     <View
       testID={props.testID}
@@ -11,9 +14,9 @@ export default function EmptyRegisters(props: Props) {
       <MaterialCommunityIcons
         name="sticker-alert-outline"
         size={60}
-        color="black"
+        color={colorScheme === "dark" ? "white" : "black"}
       />
-      <Text className="text-black text-center text-xl p-5">
+      <Text className="text-black dark:text-white text-center text-xl p-5">
         Nenhum registro cadastrado.
       </Text>
     </View>
