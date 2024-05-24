a8c3c49726c2aba42ca42436a92ae57b1f2a23b9
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-24 17:01:51 -0300

    test(unit): improvement spec of Header listRegisters with mock orientation

--- src/components/common/ListRegisters/Header/test.spec.tsx
+++ src/components/common/ListRegisters/Header/test.spec.tsx
@@ -1,11 +1,26 @@
 import { render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import store from "@store";
 import Header from "./index";
 
-describe("Header Component", () => {
-  test("should render Header", () => {
-    const { getByTestId } = render(<Header testID="list-header" />);
+jest.mock("@hooks/useOrientation", () => ({
+  __esModule: true,
+  default: () => ({
+    orientation: 1,
+    landscape: false,
+    portrait: true,
+  }),
+}));
 
-    const mainContainer = getByTestId("list-header");
-    expect(mainContainer).toBeTruthy();
-  });
+test("should render Header", () => {
+  const { getByTestId } = render(
+    <Provider store={store}>
+      <NavigationContainer>
+        <Header testID="header-list-register" type="entry" />
+      </NavigationContainer>
+    </Provider>
+  );
+
+  expect(getByTestId("header-list-register")).toBeTruthy();
 });
