9c57330a7e43673aec9d8283719a48d82f7d0cb1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-24 17:30:36 -0300

    test(unit): make props mock in Actions of Modal

--- src/components/common/Modal/Actions/test.spec.tsx
+++ src/components/common/Modal/Actions/test.spec.tsx
@@ -3,7 +3,25 @@ import Actions from "./index";
 
 describe("Actions Component", () => {
   test("should render Actions", () => {
-    const { getByTestId } = render(<Actions testID="modal-actions" />);
+    const mockCloseAction = jest.fn();
+    const mockCancelAction = jest.fn();
+    const mockConfirmAction = jest.fn();
+    const mockCloseModal = jest.fn();
+    const mockConfirmButton = { text: "Confirm", onPress: mockConfirmAction };
+    const mockCancelButton = { text: "Cancel", onPress: mockCancelAction };
+
+    const props = {
+      testID: "modal-actions",
+      twClass: "test-class",
+      closeAction: mockCloseAction,
+      cancelAction: mockCancelAction,
+      confirmAction: mockConfirmAction,
+      closeModal: mockCloseModal,
+      confirmButton: mockConfirmButton,
+      cancelButton: mockCancelButton,
+    };
+
+    const { getByTestId } = render(<Actions {...props} />);
 
     const mainContainer = getByTestId("modal-actions");
     expect(mainContainer).toBeTruthy();
