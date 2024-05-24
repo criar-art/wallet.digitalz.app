b280965d2c2e8679bcea58853f3db02ecfd5dd3e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-24 03:59:43 -0300

    create new object for filter and selectors in store

--- src/store/commonSlice/index.ts
+++ src/store/commonSlice/index.ts
@@ -1,16 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { Selector, createSelector, createSlice } from "@reduxjs/toolkit";
 import { common } from "./types";
+import { RootState } from "@store";
+import { sortDataByDateDesc } from "@utils";
 
 export const commonSlice = createSlice({
   name: "commonState",
   initialState: {
     registers: [],
     registerData: "",
+    registerFilter: {
+      short: "",
+      startDate: "",
+      endDate: "",
+      searchTerm: "",
+      pay: undefined,
+    },
     eyeStatus: false,
   } as common,
   reducers: {
     setRegister(state, payload) {
-      state.registers = payload.payload;
+      state.registers = sortDataByDateDesc(payload.payload);
     },
     setEditRegister(state, payload) {
       const itemIndex = state.registers.findIndex(
@@ -21,13 +30,52 @@ export const commonSlice = createSlice({
     setRegisterData(state, payload) {
       state.registerData = payload.payload;
     },
+    setRegisterFilter(state, payload) {
+      state.registerFilter = {
+        ...state.registerFilter,
+        ...payload.payload,
+      };
+    },
+    setResetFilter(state, payload) {
+      state.registerFilter = payload.payload;
+    },
     setEyeStatus(state, payload) {
       state.eyeStatus = payload.payload;
     },
   },
 });
 
-export const { setRegister, setEditRegister, setRegisterData, setEyeStatus } =
-  commonSlice.actions;
+// Selector para filtrar e classificar os registros com base no tipo.
+export const selectRegisters = (type: string) => (state: RootState) => {
+  const registers = state.commonState.registers;
+
+  // Filtrar os registros pelo tipo especificado
+  const filteredData = registers.filter((item) => item.type === type);
+  return sortDataByDateDesc(filteredData);
+};
+
+// Defina uma função de fábrica que aceita o tipo como parâmetro
+const makeSelectRegistersType: (
+  type: string
+) => Selector<RootState, common["registers"]> = (type: string) =>
+  createSelector(
+    (state: RootState) => state.commonState.registers,
+    (registers) => {
+      // Retorna os registros filtrados com base no tipo
+      return registers.filter((item) => item.type === type);
+    }
+  );
+
+// Agora você pode criar um seletor passando o tipo específico
+export const selectRegistersType = makeSelectRegistersType;
+
+export const {
+  setRegister,
+  setEditRegister,
+  setRegisterData,
+  setEyeStatus,
+  setRegisterFilter,
+  setResetFilter,
+} = commonSlice.actions;
 
 export default commonSlice.reducer;
--- src/store/commonSlice/types.ts
+++ src/store/commonSlice/types.ts
@@ -1,5 +1,16 @@
+interface Register {
+  id: string;
+  type: string;
+  date: string;
+  name: string;
+  value: number;
+  pay: boolean | undefined;
+  // Outros campos, se houver
+}
+
 export interface common {
-  registers: Array<any>;
+  registers: Array<Register>;
   registerData: any;
   eyeStatus: boolean;
+  registerFilter: any;
 }
