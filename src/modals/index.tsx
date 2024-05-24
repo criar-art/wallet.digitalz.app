8c47c68eaf8f37ba548d9b9cf3aab871134e25eb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-24 03:55:24 -0300

    create modalFilter in global component

--- src/modals/index.tsx
+++ src/modals/index.tsx
@@ -1,6 +1,7 @@
 import ModalRegister from "./Register";
 import ModalAlert from "./Delete";
 import ModalInfo from "./Information";
+import ModalFilter from "./Filter";
 
 export default function ModalGlobal() {
   return (
@@ -8,6 +9,7 @@ export default function ModalGlobal() {
       <ModalRegister />
       <ModalAlert />
       <ModalInfo />
+      <ModalFilter />
     </>
   );
 }
