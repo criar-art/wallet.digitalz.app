b4d46833688509dc7fcb7b62ad9c362da073d87a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-25 04:26:05 -0300

    adjustment hidden filter in one registers

--- src/components/common/ListRegisters/Header/index.tsx
+++ src/components/common/ListRegisters/Header/index.tsx
@@ -8,11 +8,14 @@ import { useAppDispatch } from "@store/hooks";
 import TotalCategory from "../../TotalCategory";
 import Button from "../../Button";
 import useOrientation from "@hooks/useOrientation";
+import { useSelector } from "react-redux";
+import { selectRegistersType } from "@store/commonSlice";
 
 export default function Header(props: Props) {
   const { colorScheme } = useColorScheme();
   const dispatch = useAppDispatch();
   const { portrait } = useOrientation();
+  const getRegisters = useSelector(selectRegistersType(String(props.type)));
 
   return (
     !!portrait && (
@@ -25,20 +28,22 @@ export default function Header(props: Props) {
           type={String(props.type)}
           onPress={() => dispatch(setModalInfo(props.type))}
         />
-        <Button
-          twClass="bg-gray-100 dark:bg-zinc-900 p-4 pr-8 mt-4 rounded-l-full absolute right-0"
-          text="Filtro"
-          label="Abrir modal de filtros"
-          textColor="text-black dark:text-white"
-          onPress={() => dispatch(setModalFilter(props.type))}
-          icon={
-            <MaterialIcons
-              name="filter-list"
-              size={26}
-              color={colorScheme === "dark" ? "white" : "black"}
-            />
-          }
-        />
+        {getRegisters.length > 1 && (
+          <Button
+            twClass="bg-gray-100 dark:bg-zinc-900 p-4 pr-8 mt-4 rounded-l-full absolute right-0"
+            text="Filtro"
+            label="Abrir modal de filtros"
+            textColor="text-black dark:text-white"
+            onPress={() => dispatch(setModalFilter(props.type))}
+            icon={
+              <MaterialIcons
+                name="filter-list"
+                size={26}
+                color={colorScheme === "dark" ? "white" : "black"}
+              />
+            }
+          />
+        )}
       </View>
     )
   );
