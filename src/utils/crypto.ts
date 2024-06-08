718a718cc2c753e81c1a9208b6e01648af961198
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-08 08:40:44 -0300

    change for  crypto-es

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,11 +1,23 @@
-import CryptoJS from "react-native-crypto-js";
-import { WALLET } from '@env';
+import CryptoES from "crypto-es";
+import { WALLET } from "@env";
 
 export const encryptData = (data: any) => {
-  return CryptoJS.AES.encrypt(JSON.stringify(data), WALLET).toString();
+  if (data === undefined || data === null) {
+    throw new Error("Data is undefined or null. Cannot encrypt.");
+  }
+  return CryptoES.AES.encrypt(JSON.stringify(data), WALLET).toString();
 };
 
 export const decryptData = (encryptedData: string) => {
-  const bytes = CryptoJS.AES.decrypt(encryptedData, WALLET);
-  return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  if (encryptedData === undefined || encryptedData === null) {
+    throw new Error("Encrypted data is undefined or null. Cannot decrypt.");
+  }
+  const bytes = CryptoES.AES.decrypt(encryptedData, WALLET);
+  const decryptedText = bytes.toString(CryptoES.enc.Utf8);
+
+  if (!decryptedText) {
+    throw new Error("Failed to decrypt data. Decrypted text is empty.");
+  }
+
+  return JSON.parse(decryptedText);
 };
