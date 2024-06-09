314a61165d39dd8c07c0f782be09bca790789d08
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-09 07:09:37 -0300

    i18n config

---
+++ src/i18n/index.ts
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as Localization from "expo-localization";
+import translationEn from "../locales/en/translation.json";
+import translationPt from "../locales/pt/translation.json";
+
+// Configuração das traduções
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  pt: {
+    translation: translationPt,
+  },
+};
+
+// Inicialização do i18next
+i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources,
+  lng: Localization.locale, // Define a língua com base na configuração do dispositivo
+  fallbackLng: "pt", // Língua padrão caso a língua do dispositivo não seja suportada
+  interpolation: {
+    escapeValue: false, // React já faz a sanitização
+  },
+});
+
+// Configura a direção do texto (LTR ou RTL)
+// I18nManager.allowRTL(false);
+// I18nManager.forceRTL(Localization.isRTL);
+
+export default i18n;
