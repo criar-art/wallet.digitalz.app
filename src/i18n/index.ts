dcc3ce0e0b624f44096edf71cf3392888e4eb21d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-11 08:25:41 -0300

    config import new languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,6 +3,9 @@ import { initReactI18next } from "react-i18next";
 import * as Localization from "expo-localization";
 import translationEn from "../locales/en/translation.json";
 import translationPt from "../locales/pt/translation.json";
+import translationRu from "../locales/ru/translation.json";
+import translationZh from "../locales/zh/translation.json";
+import translationEs from "../locales/es/translation.json";
 
 // Configuração das traduções
 const resources = {
@@ -12,11 +15,20 @@ const resources = {
   pt: {
     translation: translationPt,
   },
+  ru: {
+    translation: translationRu,
+  },
+  zh: {
+    translation: translationZh,
+  },
+  es: {
+    translation: translationEs,
+  },
 };
 
 // Inicialização do i18next
 i18n.use(initReactI18next).init({
-  compatibilityJSON: 'v3',
+  compatibilityJSON: "v3",
   resources,
   lng: Localization.locale, // Define a língua com base na configuração do dispositivo
   fallbackLng: "pt", // Língua padrão caso a língua do dispositivo não seja suportada
