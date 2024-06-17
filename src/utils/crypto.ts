435a67604a5761fadd724cd63751bc58b8bbd17c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-17 02:09:48 -0300

    adjustemnt words crypt with device modelName unique

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,18 +1,21 @@
+import * as Device from 'expo-device';
 import CryptoES from "crypto-es";
 import { WALLET } from "@env";
+
+const wallet = `${Device.modelName?.replace(/\s/g, '')}${WALLET}`;
 
 export const encryptData = (data: any) => {
   if (data === undefined || data === null) {
     throw new Error("Data is undefined or null. Cannot encrypt.");
   }
-  return CryptoES.AES.encrypt(JSON.stringify(data), WALLET).toString();
+  return CryptoES.AES.encrypt(JSON.stringify(data), wallet).toString();
 };
 
 export const decryptData = (encryptedData: string) => {
   if (encryptedData === undefined || encryptedData === null) {
     throw new Error("Encrypted data is undefined or null. Cannot decrypt.");
   }
-  const bytes = CryptoES.AES.decrypt(encryptedData, WALLET);
+  const bytes = CryptoES.AES.decrypt(encryptedData, wallet);
   const decryptedText = bytes.toString(CryptoES.enc.Utf8);
 
   if (!decryptedText) {
