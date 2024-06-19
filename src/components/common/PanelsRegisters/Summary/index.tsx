dcf525c69ff561066c4300ac002e2b6c35c0d065
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-19 10:07:47 -0300

    improvement return percentage with values interge and zero

--- src/components/common/PanelsRegisters/Summary/index.tsx
+++ src/components/common/PanelsRegisters/Summary/index.tsx
@@ -10,56 +10,85 @@ import { RootState } from "@store";
 import useOrientation from "@hooks/useOrientation";
 
 export default function Summary({ testID }: any) {
-  const common = useAppSelector((state: RootState) => state.commonState);
+  const { t } = useTranslation();
   const { getTotal } = useBalance();
   const { colorScheme } = useColorScheme();
-  const { t } = useTranslation();
   const { landscape } = useOrientation();
+  const common = useAppSelector((state: RootState) => state.commonState);
 
   const pieData = [
-    { value: getTotal("expense"), color: '#f87171' },
-    { value: getTotal("entry"), color: '#4ade80' },
-    { value: getTotal("investment"), color: '#38bdf8' },
+    { value: getTotal("expense"), color: "#f87171" },
+    { value: getTotal("entry"), color: "#4ade80" },
+    { value: getTotal("investment"), color: "#38bdf8" },
   ];
 
   const totalValue = pieData.reduce((acc, item) => acc + item.value, 0);
-  const getPercentage = (value: number) => (totalValue ? (value / totalValue) * 100 : 0).toFixed(2);
+  const getPercentage = (value: number) => {
+    if (totalValue === 0) return "0.00";
+    const percentage = (value / totalValue) * 100;
+    return percentage % 1 === 0 ? percentage.toString() : percentage.toFixed(2);
+  };
 
   const renderDot = (color: string) => (
     <View
-      className="w-3 h-3 rounded-full mr-2"
       style={{ backgroundColor: color }}
+      className="w-3 h-3 rounded-full mr-2"
     />
   );
 
-  const renderLegendItem = (color: string, label: string, percentage: string) => (
-    <View className={`flex gap-2 flex-1 items-center ${landscape ? "flex-row" : "flex-col"}`}>
+  const renderLegendItem = (
+    color: string,
+    label: string,
+    percentage: string
+  ) => (
+    <View
+      key={label}
+      className={`flex gap-2 ${
+        landscape ? "flex-row" : "flex-col"
+      } items-center flex-1`}
+    >
       <View className="flex flex-row items-center">
         {renderDot(color)}
         <Text className="text-black dark:text-white">{label}</Text>
       </View>
-      <Text className="text-black dark:text-white text-lg font-bold">{percentage}%</Text>
+      <Text className="text-black dark:text-white text-lg font-bold">
+        {percentage}%
+      </Text>
     </View>
   );
 
   const renderTitle = () => (
     <Text className="text-black dark:text-white font-bold text-center text-xl">
-      {t('common.summary')}
+      {t("common.summary")}
     </Text>
-  )
+  );
 
   const renderLegendComponent = () => (
     <View className={`flex ${landscape ? "flex-col h-28" : "flex-row"}`}>
       {landscape && renderTitle()}
-      {getPercentage(pieData[0].value) !== '0.00' && renderLegendItem('#f87171', t('common.expense'), utils.parseMoney(getPercentage(pieData[0].value), common.eyeStatus))}
-      {getPercentage(pieData[1].value) !== '0.00' && renderLegendItem('#4ade80', t('common.entry'), utils.parseMoney(getPercentage(pieData[1].value), common.eyeStatus))}
-      {getPercentage(pieData[2].value) !== '0.00' && renderLegendItem('#38bdf8', t('common.investment'), utils.parseMoney(getPercentage(pieData[2].value), common.eyeStatus))}
+      {pieData.map((item, index) => {
+        const percentage = getPercentage(item.value);
+        if (percentage !== "0" && percentage !== "0.00") {
+          return renderLegendItem(
+            item.color,
+            t(`common.${["expense", "entry", "investment"][index]}`),
+            utils.parseMoney(percentage, common.eyeStatus)
+          );
+        }
+        return null;
+      })}
     </View>
   );
 
   return (
     <View className="flex-1 m-5" testID={testID}>
-      <View className={`flex rounded-lg ${landscape ? "flex-row justify-center items-center mt-5" : "flex-col p-5 bg-white dark:bg-zinc-800 shadow-lg"}`}>
+      <View
+        className={`flex rounded-lg ${
+          landscape
+            ? "flex-row justify-center items-center mt-5"
+            : "flex-col p-5 bg-white dark:bg-zinc-800 shadow-lg"
+        }`}
+      >
         <View className="flex">
           {!landscape && renderTitle()}
           <View className={`items-center ${landscape ? "mr-10" : "p-4"}`}>
@@ -69,7 +98,7 @@ export default function Summary({ testID }: any) {
               showGradient
               radius={90}
               innerRadius={60}
-              innerCircleColor={colorScheme === "dark" ? "#27272a" : '#fff'}
+              innerCircleColor={colorScheme === "dark" ? "#27272a" : "#fff"}
               centerLabelComponent={() => (
                 <View className="flex justify-center items-center">
                   <MaterialCommunityIcons
@@ -85,9 +114,18 @@ export default function Summary({ testID }: any) {
         {renderLegendComponent()}
         {!landscape && (
           <View className="flex flex-row w-full mt-4 rounded-lg overflow-hidden">
-            <View className="h-2 bg-red-400" style={{ width: `${getPercentage(pieData[0].value)}%` } as any} />
-            <View className="h-2 bg-green-300" style={{ width: `${getPercentage(pieData[1].value)}%` } as any} />
-            <View className="h-2 bg-blue-300" style={{ width: `${getPercentage(pieData[2].value)}%` } as any} />
+            {pieData.map((item, index) => (
+              <View
+                key={index}
+                className="h-2"
+                style={
+                  {
+                    backgroundColor: item.color,
+                    width: `${getPercentage(item.value)}%`,
+                  } as any
+                }
+              />
+            ))}
           </View>
         )}
       </View>
