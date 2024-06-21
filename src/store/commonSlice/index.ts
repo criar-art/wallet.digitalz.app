ed1f5f193b19467b4ffb6107517bf752bdec71b2
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-21 16:41:53 -0300

    state of menuVisible

--- src/store/commonSlice/index.ts
+++ src/store/commonSlice/index.ts
@@ -6,6 +6,7 @@ export const commonSlice = createSlice({
   initialState: {
     registerData: "",
     eyeStatus: false,
+    menuVisible: false,
   } as CommonState,
   reducers: {
     setRegisterData(state, action: PayloadAction<string>) {
@@ -14,8 +15,12 @@ export const commonSlice = createSlice({
     setEyeStatus(state, action: PayloadAction<boolean>) {
       state.eyeStatus = action.payload;
     },
+    setMenuVisible(state, action: PayloadAction<boolean>) {
+      state.menuVisible = action.payload;
+    },
   },
 });
 
-export const { setRegisterData, setEyeStatus } = commonSlice.actions;
+export const { setRegisterData, setEyeStatus, setMenuVisible } =
+  commonSlice.actions;
 export default commonSlice.reducer;
