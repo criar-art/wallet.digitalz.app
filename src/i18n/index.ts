bee09d56ac33d5b81b1ac1c354eed235ab2bef73
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-24 16:34:26 -0300

    adjustment new imports languages in i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,39 +1,60 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import * as Localization from "expo-localization";
-import translationEn from "../locales/en/translation.json";
-import translationPt from "../locales/pt/translation.json";
-import translationRu from "../locales/ru/translation.json";
-import translationZh from "../locales/zh/translation.json";
-import translationEs from "../locales/es/translation.json";
+import translationEn from "../locales/en-US/translation.json";
+import translationPt from "../locales/pt-BR/translation.json";
+import translationRu from "../locales/ru-RU/translation.json";
+import translationZh from "../locales/zh-CN/translation.json";
+import translationEs from "../locales/es-ES/translation.json";
+import translationFr from "../locales/fr-FR/translation.json";
+import translationIt from "../locales/it-IT/translation.json";
+import translationHi from "../locales/hi-IN/translation.json";
+
+// Função para carregar as traduções de espanhol
+const loadSpanishTranslations = () => {
+  return {
+    "es-ES": { translation: translationEs },
+    "es-AR": { translation: translationEs },
+    "es-BO": { translation: translationEs },
+    "es-CL": { translation: translationEs },
+    "es-CO": { translation: translationEs },
+    "es-EC": { translation: translationEs },
+    "es-GY": { translation: translationEs },
+    "es-PY": { translation: translationEs },
+    "es-PE": { translation: translationEs },
+    "es-SR": { translation: translationEs },
+    "es-UY": { translation: translationEs },
+    "es-VE": { translation: translationEs },
+  };
+};
+
+const loadPortuguesTranslations = () => {
+  return {
+    "pt-BR": { translation: translationPt },
+    "pt-PT": { translation: translationPt },
+  };
+};
 
 // Configuração das traduções
 const resources = {
-  en: {
-    translation: translationEn,
-  },
-  pt: {
-    translation: translationPt,
-  },
-  ru: {
-    translation: translationRu,
-  },
-  zh: {
-    translation: translationZh,
-  },
-  es: {
-    translation: translationEs,
-  },
+  "en-US": { translation: translationEn },
+  "ru-RU": { translation: translationRu },
+  "zh-CN": { translation: translationZh },
+  ...loadPortuguesTranslations(),
+  ...loadSpanishTranslations(),
+  "fr-FR": { translation: translationFr },
+  "it-IT": { translation: translationIt },
+  "hi-IN": { translation: translationHi },
 };
 
 // Inicialização do i18next
 i18n.use(initReactI18next).init({
   compatibilityJSON: "v3",
   resources,
-  lng: Localization.locale, // Define a língua com base na configuração do dispositivo
-  fallbackLng: "pt", // Língua padrão caso a língua do dispositivo não seja suportada
+  lng: Localization.locale,
+  fallbackLng: "pt-BR",
   interpolation: {
-    escapeValue: false, // React já faz a sanitização
+    escapeValue: false,
   },
 });
 
