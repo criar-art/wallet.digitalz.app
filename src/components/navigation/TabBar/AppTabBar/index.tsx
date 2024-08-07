be9529ff4867952ca1d7af1aff3ad58f3532be33
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-07 07:51:38 -0300

    change appTabBar with budget

--- src/components/navigation/TabBar/AppTabBar/index.tsx
+++ src/components/navigation/TabBar/AppTabBar/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { Animated, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import useOrientation from "@hooks/useOrientation";
-import { setModalRegister } from "@store/modalsSlice";
+import { setModalBudget, setModalBudgetTransaction, setModalRegister } from "@store/modalsSlice";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import utils from "@utils";
 import Button from "@components/common/Button";
@@ -11,13 +11,19 @@ import useIsTablet from "@hooks/useIsTablet";
 import { RootState } from "@store";
 import { Props, Route } from "./types";
 
-export default function AppTabBar({ state, descriptors, navigation }: Props) {
+export default function AppTabBar({ state, descriptors, navigation, type }: Props) {
   const { landscape } = useOrientation();
   const isTablet = useIsTablet();
   const dispatch = useAppDispatch();
   const common = useAppSelector((state: RootState) => state.commonState);
+  const idBudget = state.routes[1]?.params?.id;
   const handleNewRegister = () => dispatch(setModalRegister("register"));
+  const handleNewBudget = () => dispatch(setModalBudget("budget"));
+  const handleAddMoney = () => dispatch(setModalBudgetTransaction({ id: idBudget, type: 'add' }));
+  // const handleAddMoney = () => console.log("test");
   const menuTranslateY = useRef(new Animated.Value(0)).current;
+
+  console.log(idBudget)
 
   useEffect(() => {
     if (common.menuVisible) {
@@ -41,21 +47,33 @@ export default function AppTabBar({ state, descriptors, navigation }: Props) {
       className={`absolute bottom-0 flex bg-transparent dark:bg-zinc-900 flex-row justify-center ${
         landscape ? "h-0 left-[50%]" : ""
       }`}
-      style={[{ transform: [{ translateY: menuTranslateY }] }]}
+      style={type == 'wallet' && [{ transform: [{ translateY: menuTranslateY }] }]}
     >
       <View
         className={`flex flex-row ${
           landscape || isTablet ? "w-[400] absolute bottom-0" : "w-full"
         }`}
       >
-        <Button
-          pressableButton
-          label="Novo registro"
-          textColor="text-white"
-          twClass={`z-10 rounded-full absolute w-18 h-18 border-2 border-white dark:border-zinc-900 bg-green-600 bottom-[44] left-1/2 -translate-x-7 translate-y-2`}
-          onPress={handleNewRegister}
-          icon={<MaterialIcons name="add" size={35} color="white" />}
-        />
+        {type == 'wallet' && (
+          <Button
+            pressableButton
+            label="Novo registro"
+            textColor="text-white"
+            twClass={`z-10 rounded-full absolute w-18 h-18 border-2 border-white dark:border-zinc-900 bg-green-600 bottom-[44] left-1/2 -translate-x-7 translate-y-2`}
+            onPress={handleNewRegister}
+            icon={<MaterialIcons name="add" size={35} color="white" />}
+          />
+        )}
+        {type == 'budget' && (
+          <Button
+            pressableButton
+            label="Novo budget"
+            textColor="text-white"
+            twClass={`z-10 rounded-full absolute w-18 h-18 border-2 border-white dark:border-zinc-900 bg-green-600 bottom-[44] left-1/2 -translate-x-7 translate-y-2`}
+            onPress={state.index === 0 ? handleNewBudget : handleAddMoney}
+            icon={<MaterialIcons name="add" size={35} color="white" />}
+          />
+        )}
         {state?.routes.map((route: Route, index: number) => {
           const { options } = descriptors[route.key];
           const labelButton: string = utils.getLabel(options, route);
@@ -79,6 +97,10 @@ export default function AppTabBar({ state, descriptors, navigation }: Props) {
               target: route.key,
             });
           };
+
+          if(route.name == 'Transaction') {
+            return;
+          }
 
           return (
             <AppTabButton
